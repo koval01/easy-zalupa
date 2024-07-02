0c3ca2c17a778961d3c6247b3a429a96968c823a
ksvarog2004 <ksvarog2004@gmail.com>
2024-07-02 14:05:30 +0000

    Cloudflare pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
+}
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
