f24fc7d63967ede0bad4908edebaa899f10c9faa
ksvarog2004 <ksvarog2004@gmail.com>
2024-07-02 14:29:35 +0000

    Remove ratelimit

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import rateLimit from "@/utils/rate-limit";
 
 export const runtime = 'edge';
 
@@ -8,27 +7,17 @@ type ResponseData = {
   response?: []
 }
 
-const limiter = rateLimit({
-  interval: 60 * 1000, // 60 seconds
-  uniqueTokenPerInterval: 500, // Max 500 users per second
-});
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   try {
-    await limiter.check(res, 50, "API_PRODUCTS_RATE");
-    try {
-      const response = await fetch('https://easydonate.ru/api/v3/shop/products', {
-        headers: { "Shop-Key": process.env.API_KEY || "" }
-      });
-      const data: ResponseData = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(200).json({ success: false });
-    }
-  } catch {
-    res.status(429).json({ success: false });
+    const response = await fetch('https://easydonate.ru/api/v3/shop/products', {
+      headers: { "Shop-Key": process.env.API_KEY || "" }
+    });
+    const data: ResponseData = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(200).json({ success: false });
   }
 }
